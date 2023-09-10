fa11a699035867b47b4ad421db2e88df80d00a2e
Pedro Zich <zichpedro22@gmail.com>
2023-09-10 15:25:34 -0300

    feat: user avatar provider

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -11,17 +11,28 @@
 // }
 // usersRoutes.use(myMiddleware);
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 //POST
 usersRoutes.post("/", usersController.create);
 //quando acessar essa rota é interceptado pelo middleware de autenticação
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+);
 
 module.exports = usersRoutes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload");
 
 const express = require("express");
 const routes = require("./routes");
@@ -9,34 +10,35 @@ migrationsRun();
 
 const app = express();
 app.use(express.json());
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 
 app.use((error, request, response, next) => {
-	if (error instanceof AppError)
-		return response.status(error.statusCode).json({
-			status: "error",
-			message: error.message,
-		});
+  if (error instanceof AppError)
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
 
-	return response.status(500).json({
-		status: "error",
-		message: "Internal server error",
-	});
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
 });
 
 // GET
 // route params: obrigatório(faz parte da rota)
 app.get("/message/:id/:user", (request, response) => {
-	const { id, user } = request.params;
-	response.send(`Id da mensagem: ${id}. Para o usuário: ${user}`);
+  const { id, user } = request.params;
+  response.send(`Id da mensagem: ${id}. Para o usuário: ${user}`);
 });
 
 //query params: opcional
 app.get("/users", (request, response) => {
-	const { page, limit } = request.query;
+  const { page, limit } = request.query;
 
-	response.send(`Página: ${page}. Mostrar: ${limit}`);
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
 });
