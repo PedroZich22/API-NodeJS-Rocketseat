eeeefd6fcb08d55e289574ac59850354c805cc2d
Pedro Zich <zichpedro22@gmail.com>
2023-09-10 14:27:44 -0300

    feat: auth middleware for user id

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,4 @@
 //Rota: receber a requisição e a resposta e repassar para o controller equivalente
-const { Router } = require("express");
-
-const UsersController = require("../controllers/UsersController");
-
-const usersRoutes = Router();
 
 //Middleware: interceptadores de requisições
 // conhecem a requisição, seu corpo, sua resposta e o destino da requisição
@@ -14,12 +9,19 @@ const usersRoutes = Router();
 
 // 	next();
 // }
-
 // usersRoutes.use(myMiddleware);
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const usersRoutes = Router();
+
 const usersController = new UsersController();
 
 //POST
 usersRoutes.post("/", usersController.create);
-usersRoutes.put("/:id", usersController.update);
+//quando acessar essa rota é interceptado pelo middleware de autenticação
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
 
 module.exports = usersRoutes;
